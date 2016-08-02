ce345ea8098edf2dabd4766f5c58ca86d60f0706
holyoke <jasonebueng@gmail.com>
2016-08-02 15:11:11 -0700

    Add db level check for null

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  license    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Project < ApplicationRecord
   validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802213031) do
+ActiveRecord::Schema.define(version: 20160802220838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "projects", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",       null: false
     t.string   "license"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
