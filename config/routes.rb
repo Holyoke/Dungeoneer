fbd23311164ba0673ad9a7ea7172381c2904c528
holyoke <jasonebueng@gmail.com>
2016-08-02 20:36:56 -0700

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :areas
-  resources :projects
+  resources :projects, only: [:index, :create, :show] do
+    resources :areas
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
