2026d4509962107d5184d323837aab37ce75881a
holyoke <jasonebueng@gmail.com>
2016-08-04 14:46:46 -0700

    Clean up Areas#Show

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,37 +1,36 @@
 class AreasController < ApplicationController
-  before_action :set_area, only: [:show, :edit, :update, :destroy]
+  before_filter :set_project
 
   def index
-    @areas = Area.all
+    @areas = @project.areas
   end
 
   # GET /areas/new
   def new
-    @area = Area.new
+    @area = @project.areas.new
     render :new
   end
 
-  # GET /areas/1
-  # GET /areas/1.json
   def show
+    @area = @project.areas.find(params[:id])
   end
 
   # GET /areas/1/edit
   def edit
+    @area = @project.areas.find(params[:id])
   end
 
   # POST /areas
   # POST /areas.json
   def create
-    project = Project.find(area_params[:project_id])
-    @area = project.areas.new(area_params)
+    @area = @project.areas.new(area_params)
 
     respond_to do |format|
       if @area.save
-        format.html { redirect_to @area, notice: 'Area was successfully created.' }
+        format.html { redirect_to [@project, @area], notice: 'Area was successfully created.' }
         format.json { render :show, status: :created, location: @area }
       else
-        format.html { render new_project_area_url(@area.project) }
+        format.html { render :new }
         format.json { render json: @area.errors, status: :unprocessable_entity }
       end
     end
@@ -40,9 +39,11 @@ class AreasController < ApplicationController
   # PATCH/PUT /areas/1
   # PATCH/PUT /areas/1.json
   def update
+    @area = @project.areas.find(params[:id])
+
     respond_to do |format|
       if @area.update(area_params)
-        format.html { redirect_to @area.project, notice: 'Area was successfully updated.' }
+        format.html { redirect_to [@project, @area], notice: 'Area was successfully updated.' }
         format.json { render :show, status: :ok, location: @area }
       else
         format.html { render :edit }
@@ -54,20 +55,26 @@ class AreasController < ApplicationController
   # DELETE /areas/1
   # DELETE /areas/1.json
   def destroy
+    @area = @project.areas.find(params[:id])
+
     @area.destroy
     respond_to do |format|
-      format.html { redirect_to areas_url, notice: 'Area was successfully destroyed.' }
+      format.html { redirect_to project_areas_path(@project), notice: 'Area was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_area
-      @area = Area.find(params[:id])
+    def set_project
+      if params.has_key?(:project_id)
+        @project = Project.find(params[:project_id])
+      else
+        @project = Area.find(params[:id]).project
+      end
+
+      @project
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def area_params
       params.require(:area).permit(:name, :floor_plan, :project_id)
     end
