2cedae5c17af1d13186438a0465487ae59c277b3
holyoke <jasonebueng@gmail.com>
2016-08-10 14:30:12 -0700

    Sets up more initial user sign in specs

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -5,4 +5,22 @@ feature 'Sign in', :devise do
      sign_in('test@test.com', 'password')
      expect(page).to have_content 'Invalid email or password'
   end
+
+  scenario 'user can sign in with valid credentials' do
+    user = FactoryGirl.create(:user)
+    sign_in(user.email, user.password)
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'user cannot sign in with an invalid email' do
+    user = FactoryGirl.create(:user)
+    sign_in('invalid@email.com', user.password)
+    expect(page).to have_content 'Invalid email or password'
+  end
+
+  scenario 'user cannot sign in with an invalid password' do
+    user = FactoryGirl.create(:user)
+    sign_in(user.email, 'invalidPassword')
+    expect(page).to have_content 'Invalid email or password'
+  end
 end
