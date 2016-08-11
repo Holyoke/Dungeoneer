7386f25debf1c758e608f408a47d495e5f57054a
holyoke <jasonebueng@gmail.com>
2016-08-11 16:16:21 -0700

    sets up pundit

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,13 @@
+class User
+  attr_reader :current, :model
+
+  def initialize(current_user, model)
+    @current_user = current_user
+    @user = model
+  end
+
+  def index?
+    @current_user.admin?
+  end
+
+end
