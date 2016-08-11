478baebb8ef5df807154b6d75409195672f69c2d
holyoke <jasonebueng@gmail.com>
2016-08-11 14:42:31 -0700

    Adds role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+
+  enum role: [:user, :collaborator, :admin]
+
+  after_initialize :set:default_role, :if => :new_record?
+
+  def set_default_role
+    set.role || :user
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   root 'static_pages#root'
-  
+
   resources :areas, except: [:index]
 
   resources :projects do
