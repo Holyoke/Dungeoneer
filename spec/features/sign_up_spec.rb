6a2ba1fbdc39b34c341b8d41968ea9d5096de034
holyoke <jasonebueng@gmail.com>
2016-08-31 20:00:28 -0700

    fixes sign in spec typo

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Sign in', :devise do
   scenario 'user cannot login if not registered' do
      sign_in('test@test.com', 'password')
-     expect(page).to have_content 'Invalid Email or password.'
+     expect(page).to have_content 'Invalid email or password.'
   end
 
   scenario 'user can sign in with valid credentials' do
@@ -15,12 +15,12 @@ feature 'Sign in', :devise do
   scenario 'user cannot sign in with an invalid email' do
     user = FactoryGirl.create(:user)
     sign_in('invalid@email.com', user.password)
-    expect(page).to have_content 'Invalid Email or password.'
+    expect(page).to have_content 'Invalid email or password.'
   end
 
   scenario 'user cannot sign in with an invalid password' do
     user = FactoryGirl.create(:user)
     sign_in(user.email, 'invalidPassword')
-    expect(page).to have_content 'Invalid Email or password.'
+    expect(page).to have_content 'Invalid email or password.'
   end
 end
