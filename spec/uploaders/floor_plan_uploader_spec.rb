9a58b7c4de03afce1f26e344f0c58c4e9103e764
holyoke <jasonebueng@gmail.com>
2016-09-08 15:06:05 -0700

    adds floor plan uploader spec

---
+++ spec/uploaders/floor_plan_uploader_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+require 'carrierwave/test/matchers'
+
+describe FloorPlanUploader do
+  include CarrierWave::Test::Matchers
+
+  let(:area) { FactoryGirl.build(:area) }
+  let(:uploader) { FloorPlanUploader.new(area, :floor_plan) }
+
+  before do
+    FloorPlanUploader.enable_processing = true
+    File.open(File.join(Rails.root, 'spec', 'sample_data', 'sample_floor_plan_small.pdf')) { |f| uploader.store!(f) }
+  end
+
+  after do
+    FloorPlanUploader.enable_processing = false
+    uploader.remove!
+  end
+
+  context 'the full map version' do
+    it "converts a pdf into a png" do
+      expect(uploader.full_map).to be_format('png')
+    end
+  end
+end
