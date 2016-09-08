ce397039f4b701cb1f095df116450771f1b4aff1
holyoke <jasonebueng@gmail.com>
2016-09-08 14:41:30 -0700

    adds basic area model spec, shoulda matchers

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -15,5 +15,6 @@
 require 'rails_helper'
 
 RSpec.describe Area, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should belong_to(:project) }
 end
