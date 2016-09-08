7181fd70d51eea59163ce3aaabe9d99026c48733
holyoke <jasonebueng@gmail.com>
2016-09-08 14:12:44 -0700

    removes pundit and role from user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907210857) do
+ActiveRecord::Schema.define(version: 20160908042624) do
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +69,6 @@ ActiveRecord::Schema.define(version: 20160907210857) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                                     null: false
     t.datetime "updated_at",                                     null: false
-    t.integer  "role"
     t.string   "name",                              default: ""
     t.string   "authentication_token",   limit: 30
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
