86a5dbd41bba2563d3c5de4b4bab0940328bf2f5
Kam Leung <Kamleung911@gmail.com>
2016-09-09 21:51:56 -0700

    Setup leaflet and navigation for image viewing
    [#129665505]

--- app/assets/javascripts/areas.js
+++ app/assets/javascripts/areas.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var map = L.map('map', {
+  crs: L.CRS.Simple,
+  maxZoom: 4
+});
+
+var bounds = [[0,0], [1000,1000]];
+var image = L.imageOverlay($('#map').data()["floorPlanUrl"] , bounds).addTo(map);
+map.fitBounds(bounds);
