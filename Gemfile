c3d75877460bd50e6f01e8cf79d56acd8dc1a40f
holyoke <jasonebueng@gmail.com>
2016-11-10 11:19:03 -0800

    updates frontend environment

--- Gemfile
+++ Gemfile
@@ -17,7 +17,6 @@ gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'active_model_serializers', '~> 0.10.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
-gem 'turbolinks', '~> 5'
 
 #For imageuploading
 gem 'carrierwave', '~> 0.10.0'
--- frontend/webpack_config.js
+++ frontend/webpack_config.js
@@ -1,36 +1,23 @@
-var path = require('path')
-var webpack = require('webpack')
-var gutil = require('gutil')
-
-var watch_env = process.env['RAILS_ENV'] == 'production' ? false : true;
-
-webpack({
-  context: __dirname,
-  entry: "./src/entry",
-  resolve: {
-    extensions: [".jsx", ".js", ""]
-  },
+module.exports = {
+  entry: 'path/to/index.jsx',
   output: {
-    path: path.join(__dirname, '..', 'app', 'assets', 'javascripts'),
-    filename: "bundle.js"
+    path: 'app/assets/javascripts',
+    filename: 'bundle.js',
   },
-  watch: watch_env,
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
         loader: 'babel',
         query: {
           presets: ['es2015', 'react']
         }
       }
     ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx' ]
   }
-}, function(err, stats) {
-  if(err) throw new gutil.PluginError("webpack", err);
-  //Configures logging
-  gutil.log("[webpack]", stats.toString({
-  }));
-});
-
+};
