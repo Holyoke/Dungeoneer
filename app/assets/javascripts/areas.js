2d0960bafd0e4afa02f85b31e801010bbba22d29
holyoke <jasonebueng@gmail.com>
2016-11-09 17:53:07 -0800

    establishes basic marker code

--- app/assets/javascripts/areas.js
+++ app/assets/javascripts/areas.js
@@ -19,6 +19,22 @@ var height_unit = height/ratio * 1000;
 var bounds = [[0,0], [height_unit, width_unit]];
 // var bounds = [[0,0], [100,100]];
 var image = L.imageOverlay(floorPlanData["floorPlanUrl"] , bounds).addTo(map);
+console.log('Bounds:', height_unit, width_unit);
+//add markers
+var greenIcon = L.icon({
+    iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
+    shadowUrl: 'http://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+L.marker([height_unit * 1.00 , width_unit * .5], {icon: greenIcon}).addTo(map);
+L.marker([height_unit * 0.00 , width_unit * .5], {icon: greenIcon}).addTo(map);
+
+L.marker([height_unit * 0.50 , width_unit * 1.0], {icon: greenIcon}).addTo(map);
+L.marker([height_unit * 0.50 , width_unit * 0.0], {icon: greenIcon}).addTo(map);
 map.fitBounds(bounds);
-
-
